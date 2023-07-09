409484095d4484d386ddce303465e38477699884
ProWo2023 <139037654+ProWo2023@users.noreply.github.com>
2023-07-09 08:50:12 +0200

    Create ProWo2023.js

---
+++ ProWo2023.js
@@ -0,0 +1,16 @@
+function riddle00_01(){
+  //document.getElementById('id1').style.color = 'red';
+
+  wert1=document.getElementById('enterCode').value ;
+  wert=wert1.trim();
+  //document.getElementById('ausgabe').innerHTML = wert ;
+  
+  if (wert=='1234') {
+    document.getElementById('richtig').style.visibility= 'visible' ;
+    document.getElementById('falsch').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch').style.visibility= 'visible' ;
+    document.getElementById('richtig').style.visibility= 'hidden' ;
+  }
+  
+}
