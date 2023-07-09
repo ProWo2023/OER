ccd0eef316c24f6048a38aa0308abd1ef199e425
ProWo2023 <139037654+ProWo2023@users.noreply.github.com>
2023-07-09 09:41:25 +0200

    Update ProWo2023.js

--- ProWo2023.js
+++ ProWo2023.js
@@ -1,10 +1,6 @@
 function riddle00_01(){
-  //document.getElementById('id1').style.color = 'red';
-
-  wert1=document.getElementById('enterCode').value ;
-  wert=wert1.trim();
-  //document.getElementById('ausgabe').innerHTML = wert ;
-  
+  wert=document.getElementById('enterCode').value.trim() ;
+   
   if (wert=='1234') {
     document.getElementById('richtig').style.visibility= 'visible' ;
     document.getElementById('falsch').style.visibility= 'hidden' ;
@@ -12,5 +8,16 @@ function riddle00_01(){
     document.getElementById('falsch').style.visibility= 'visible' ;
     document.getElementById('richtig').style.visibility= 'hidden' ;
   }
-  
+}
+
+function riddle00_02(){
+  wert=document.getElementById('enterCode').value.trim() ;
+   
+  if (wert=='erk') {
+    document.getElementById('richtig').style.visibility= 'visible' ;
+    document.getElementById('falsch').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch').style.visibility= 'visible' ;
+    document.getElementById('richtig').style.visibility= 'hidden' ;
+  }
 }
