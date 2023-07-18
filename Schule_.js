22417ff431c36c685761d42a109bf2016010447d
ProWo2023 <139037654+ProWo2023@users.noreply.github.com>
2023-07-18 20:32:01 +0200

    Update Schule_.js

--- Schule_.js
+++ Schule_.js
@@ -1,1 +1,88 @@
+// 
+function riddle02_01(){
+  wert1=document.getElementById('enterCode').value ;
+  wert=wert1.trim();
+    
+  if (wert=='') {
+    document.getElementById('richtig').style.visibility= 'visible' ;
+    document.getElementById('falsch').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch').style.visibility= 'visible' ;
+    document.getElementById('richtig').style.visibility= 'hidden' ;
+  }
+  
+}
 
+// function riddle02_02(){
+  wert1=document.getElementById('enterCode').value ;
+  wert=wert1.trim();
+  
+  if (wert=='') {
+    document.getElementById('richtig1').style.visibility= 'visible' ;
+    document.getElementById('falsch1').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch1').style.visibility= 'visible' ;
+    document.getElementById('richtig1').style.visibility= 'hidden' ;
+  }
+  
+}
+
+// 
+function riddle02_03(){
+  wert1=document.getElementById('enterCode').value ;
+  wert=wert1.trim();
+  
+  if (wert=='') {
+    document.getElementById('richtig1').style.visibility= 'visible' ;
+    document.getElementById('falsch1').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch1').style.visibility= 'visible' ;
+    document.getElementById('richtig1').style.visibility= 'hidden' ;
+  }
+  
+}
+
+// 
+function riddle02_04(){
+  wert1=document.getElementById('enterCode').value ;
+  wert=wert1.trim();
+  
+  if (wert=='') {
+    document.getElementById('richtig1').style.visibility= 'visible' ;
+    document.getElementById('falsch1').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch1').style.visibility= 'visible' ;
+    document.getElementById('richtig1').style.visibility= 'hidden' ;
+  }
+  
+}
+
+// 
+function riddle02_05(){
+  wert1=document.getElementById('enterCode').value ;
+  wert=wert1.trim();
+  
+  if (wert=='') {
+    document.getElementById('richtig1').style.visibility= 'visible' ;
+    document.getElementById('falsch1').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch1').style.visibility= 'visible' ;
+    document.getElementById('richtig1').style.visibility= 'hidden' ;
+  }
+  
+}
+
+// 
+function riddle02_06(){
+  wert1=document.getElementById('enterCode').value ;
+  wert=wert1.trim();
+  
+  if (wert=='') {
+    document.getElementById('richtig1').style.visibility= 'visible' ;
+    document.getElementById('falsch1').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch1').style.visibility= 'visible' ;
+    document.getElementById('richtig1').style.visibility= 'hidden' ;
+  }
+  
+}
