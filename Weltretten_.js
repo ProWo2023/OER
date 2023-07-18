127578b27a6785cd7a29a530a17da20ce5412556
ProWo2023 <139037654+ProWo2023@users.noreply.github.com>
2023-07-18 20:32:57 +0200

    Update Weltretten_.js

--- Weltretten_.js
+++ Weltretten_.js
@@ -1,1 +1,58 @@
+// 
+function riddle01_01(){
+  wert1=document.getElementById('enterCode').value ;
+  wert=wert1.trim();
+    
+  if (wert=='') {
+    document.getElementById('richtig').style.visibility= 'visible' ;
+    document.getElementById('falsch').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch').style.visibility= 'visible' ;
+    document.getElementById('richtig').style.visibility= 'hidden' ;
+  }
+  
+}
 
+// function riddle01_02(){
+  wert1=document.getElementById('enterCode').value ;
+  wert=wert1.trim();
+  
+  if (wert=='') {
+    document.getElementById('richtig1').style.visibility= 'visible' ;
+    document.getElementById('falsch1').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch1').style.visibility= 'visible' ;
+    document.getElementById('richtig1').style.visibility= 'hidden' ;
+  }
+  
+}
+
+// 
+function riddle01_03(){
+  wert1=document.getElementById('enterCode').value ;
+  wert=wert1.trim();
+  
+  if (wert=='') {
+    document.getElementById('richtig1').style.visibility= 'visible' ;
+    document.getElementById('falsch1').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch1').style.visibility= 'visible' ;
+    document.getElementById('richtig1').style.visibility= 'hidden' ;
+  }
+  
+}
+
+// 
+function riddle01_04(){
+  wert1=document.getElementById('enterCode').value ;
+  wert=wert1.trim();
+  
+  if (wert=='') {
+    document.getElementById('richtig1').style.visibility= 'visible' ;
+    document.getElementById('falsch1').style.visibility= 'hidden' ;
+  } else {
+    document.getElementById('falsch1').style.visibility= 'visible' ;
+    document.getElementById('richtig1').style.visibility= 'hidden' ;
+  }
+  
+}
